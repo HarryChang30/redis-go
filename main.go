395c75d9b2eace706604b16084f8f5412ba2b813
Harry Wijaya <harry.wijaya@ovo.id>
2020-09-05 13:47:01 +0700

    cleanup

--- main.go
+++ main.go
@@ -10,66 +10,60 @@ import (
 
 var ctx = context.Background()
 
+type redisConfig struct {
+	key string
+	value string
+	expiry int
+	pipeExpiry int
+	loopSize int
+}
+
 func main() {
 	fmt.Println("Go redis implementation")
 	rdb := redisgo.ConnectClient()
 
-	// Input Scanner key and value to stored on redis
-	var redisKey string
-	var redisValue string
-	var redisExpiry int
-
-	fmt.Print("Enter key: ")
-	fmt.Scanln(&redisKey)
-
-	fmt.Print("Enter value: ")
-	fmt.Scanln(&redisValue)
-
-	fmt.Print("Enter expiry: ")
-	fmt.Scanln(&redisExpiry)
+	redis := redisConfig {
+		"setup-key",
+		"setup-value",
+		3000,
+		3000,
+		100,
+	}
 
 	// Set key
 	setNow := time.Now()
-	redisgo.Set(ctx, redisKey, redisValue, time.Duration(redisExpiry)*time.Second, rdb)
+	redisgo.Set(ctx, redis.key, redis.value, time.Duration(redis.expiry)*time.Second, rdb)
 	log.Printf("Processing set redis for single command: %+v\n", time.Since(setNow))
 
 	// Get key
 	getNow := time.Now()
-	val, err := redisgo.Get(ctx, redisKey, rdb)
+	val, err := redisgo.Get(ctx, redis.key, rdb)
 	if err != nil {
-		fmt.Printf("Key %v in redis does not exists\n", redisKey)
+		fmt.Printf("Key %v in redis does not exists\n", redis.key)
 	} else {
-		fmt.Printf("Key %v in redis exists with value %v\n", redisKey, val)
+		fmt.Printf("Key %v in redis exists with value %v\n", redis.key, val)
 	}
 	log.Printf("Processing get redis for single command: %+v\n", time.Since(getNow))
 
 	// Massive Single Command Set key
-	var loopSize int
-	fmt.Print("Enter loop size: ")
-	fmt.Scanln(&loopSize)
-
 	setMassiveNow := time.Now()
-	redisgo.SingleMassiveSet(ctx, redisKey, redisValue, time.Duration(redisExpiry)*time.Second, loopSize, rdb)
+	redisgo.SingleMassiveSet(ctx, redis.key, redis.value, time.Duration(redis.expiry)*time.Second, redis.loopSize, rdb)
 	log.Printf("Processing set redis massive for single command: %+v\n", time.Since(setMassiveNow))
 
 	// Pipeline single execute command line
-	var expirationPipe int
-	fmt.Print("Enter expiration pipeline: ")
-	fmt.Scanln(&expirationPipe)
-
 	setPipelineNow := time.Now()
-	redisgo.Pipeline(ctx, redisKey, time.Duration(expirationPipe)*time.Second, rdb)
+	redisgo.Pipeline(ctx, "setup-pipe-redis", time.Duration(redis.pipeExpiry)*time.Second, rdb)
 	log.Printf("Processing pipeline: %+v\n", time.Since(setPipelineNow))
 
 	// Pipeline single execute multiple command line
 	setMassivePipeline := time.Now()
 	n := map[string]string{
-		"harry":  "12345",
-		"tonny":  "123",
-		"adsa":   "123123asd",
-		"asdad":  "12314",
-		"czxczc": "414123",
+		"pipe-redis-1":  "pipe-value-1",
+		"pipe-redis-2":  "pipe-value-2",
+		"pipe-redis-3":   "pipe-value-3",
+		"pipe-redis-4":  "pipe-value-4",
+		"pipe-redis-5": "pipe-value-5",
 	}
-	redisgo.PipelineMassiveInsert(ctx, n, time.Duration(expirationPipe)*time.Second, rdb)
+	redisgo.PipelineMassiveInsert(ctx, n, time.Duration(redis.pipeExpiry)*time.Second, rdb)
 	log.Printf("Processing multiple set key value massive pipeline: %+v\n", time.Since(setMassivePipeline))
 }
